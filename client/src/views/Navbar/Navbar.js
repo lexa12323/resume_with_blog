f80eb28c8f38584a6a3945c4c5677b6b3e9b1ee9
Alexey <lexa12323@gmail.com>
2021-02-24 15:46:12 +0200

    make mobile friendly and burger menu

--- client/src/views/Navbar/Navbar.js
+++ client/src/views/Navbar/Navbar.js
@@ -1,29 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import {  logout  } from '../../actions/auth'
+import { Icon } from '../common/Icon/Icon';
 import { useDispatch, useSelector } from 'react-redux'
 import './navbar.scss'
+
+const Burger = ({setOpened, opened}) => {
+    return (
+        <div className={`burger ${opened && 'burger-opened'}`} onClick={() => setOpened(!opened)}>
+            <div />
+            <div />
+            <div />
+        </div>
+    )
+}
 
 export const Navbar = ({list}) => {
     const user = useSelector((state) => state.auth)
     const dispatch = useDispatch()
+    const [opened, setOpened] = useState(false)
 
     return (
         <div className="navbar">
-            <ul className="navbar__list">
+            <Burger  opened={opened} setOpened={setOpened}/>
+            <ul className={`navbar__list ${opened && 'navbar__list-opened'}`}>
                 {list.map((item, i) => 
                     <li key={i} className="navbar__item">
                         <Link to={item.href} className="navbar__link">{item.name}</Link>
                     </li>
                 )}
             
-                {/* user.authData ? (
+                { user.authData && (
                     <div>
                         <div>Logged {user?.authData?.result?.email}</div>
                         <button onClick={() => dispatch(logout())}>Logout</button>
                     </div>
-                ) : (
-                    <Link to="/auth" className="navbar__link">Login</Link>
+                ) /*: (
+                    <Link to="/auth" className="navbar__link">Login</Link> 
                 )*/}
             </ul>
         </div>
