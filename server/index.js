355d95c328b2401ba8bba62df8b56adf923597f7
dfg <dfg>
2021-03-01 17:42:27 +0200

    add production flag

--- server/index.js
+++ server/index.js
@@ -26,7 +26,11 @@ app.get('/', (req, res) => {
     res.send('Index Page')
 })
 
-const PORT = process.env.PORT || 5000
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+}
+
+const PORT = process.env.PORT || 8080
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}) 
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
