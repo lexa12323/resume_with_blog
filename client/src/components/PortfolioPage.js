d4dfa77a53d26ea4552cbffa0a7277c8a8973680
dfg <dfg>
2021-03-15 10:47:28 +0200

    sort projects

--- client/src/components/PortfolioPage.js
+++ client/src/components/PortfolioPage.js
@@ -7,15 +7,15 @@ import { PortfolioList } from '../views/Portfolio/Portfolio'
 
 const data = [
     {image: 'gorbenko_resume.png', name: 'Сайт, на котором вы находитесь', description: 'Стек технологий: MERN (mongoDB, ExpressJS, React, NodeJS). Фронтенд: React в связке с Redux. Бекенд: NodeJS CRUD App.'},
-    {image: 'joypup.png', name: 'Joy Pup', link: 'https://joy-pup.com/en/', description: 'Сайт разработан на CMS Wordpress + нативный Javascript на фронте. Сборка фронтенда: webpack, gulp. Для проекта написаны собственные плагины.'},
     {image: 'private.png', name: 'Конфиденциальные проекты', description: 'Разработка фронтенд части проектов на стеке React + Redux. Использование методологии atomic design, styled components для стилизации'},
+    {image: 'lovelama.png', name: 'LoveLama', link: 'https://lovelama.ru', description: 'Разработка только фронтенд части. Клиент отказался от разработки SPA, поэтому использовался нативный JS c библиотекой jQuery. Для отрисовки фронта использовался pug. Чат между юзерами реализован на Vue.js (компоненты) и websocket.io'},
+    {image: 'addslots.png', name: 'Addslots', description: 'Сайт разработан на PHP фреймворке Laravel (как видимимая пользователю часть так и CMS). Для отрисовки фронтенда использовался pug. На сервере использовался ffmpeg для обработки загружаемых видеофайлов. Были попытки использвать Amazon web services(S3, elastic transcoder), но позже отказались от идеи из-за цены. Пока не запущен и поставлен на паузу.'},
+    {image: 'joypup.png', name: 'Joy Pup', link: 'https://joy-pup.com/en/', description: 'Сайт разработан на CMS Wordpress + нативный Javascript на фронте. Сборка фронтенда: webpack, gulp. Для проекта написаны собственные плагины.'},
     {image: 'monolit.png', name: 'Monolit', link: 'https://nsk-monolit.ru/', description: 'Сайт разработан на CMS Wordpress. Сборка фронтенда: gulp'},
     {image: 'osn6.png', name: 'ОСН 6', link: 'https://osn-group.ru/', description: 'Сайт разработан на CMS Wordpress. Сборка фронтенда: gulp'},
     {image: 'simprint.png', name: 'Simprint', link: 'https://simprint.com.ua/', description: 'Сайт разработан на PHP фреймворке Laravel (как видимимая пользователю часть так и CMS). Для отрисовки фронтенда использовался pug.'},
-    {image: 'lovelama.png', name: 'LoveLama', link: 'https://lovelama.ru', description: 'Разработка только фронтенд части. Клиент отказался от разработки SPA, поэтому использовался нативный JS c библиотекой jQuery. Для отрисовки фронта использовался pug. Чат между юзерами реализован на Vue.js (компоненты) и websocket.io'},
     {image: 'inforoom.png', name: 'Іnfo room', link: 'https://inforoom.com.ua/', description: 'Сайт разработан на CMS Wordpress. Сборка фронтенда: gulp'},
     {image: 'siteup.png', name: 'Siteup', description: 'Сайт разработан на PHP фреймворке Laravel (как видимимая пользователю часть так и CMS). Для отрисовки фронтенда использовался pug. Использовался React для разработки отдельных компонентов (форма обратной связи с телеграм ботом для уведомления в Telegram)'},
-    {image: 'addslots.png', name: 'Addslots', description: 'Сайт разработан на PHP фреймворке Laravel (как видимимая пользователю часть так и CMS). Для отрисовки фронтенда использовался pug. На сервере использовался ffmpeg для обработки загружаемых видеофайлов. Были попытки использвать Amazon web services(S3, elastic transcoder), но позже отказались от идеи из-за цены. Пока не запущен и поставлен на паузу.'},
     {image: 'blankgun.png', name: 'Blankgun', description: 'Сайт разработан на CMS Wordpress + Woocommerce. Бюджетный проект, поэтому использует готовую тему и плагины'},
 ]
 
